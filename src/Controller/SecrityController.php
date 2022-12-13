ec7ef53cc19d5581cb50f0fff79864989c558859
PaulEtignard <Paul.etignard@gmail.Com>
2022-12-14 00:28:38 +0100

    SUpression de LoginController

--- src/Controller/SecrityController.php
+++ src/Controller/SecrityController.php
@@ -2,12 +2,30 @@
 
 namespace App\Controller;
 
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecrityController extends AbstractController
 {
+
+    #[Route('/login', name: 'app_login')]
+    public function index(AuthenticationUtils $authenticationUtils): Response
+    {
+
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+        return $this->render('login/index.html.twig', [
+            'last_username' => $lastUsername,
+            'error'         => $error,
+        ]);
+
+    }
     #[Route('/logout', name: 'app_logout', methods: ['GET'])]
     public function logout()
     {
