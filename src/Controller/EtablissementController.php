0990603f63a8eca9e9cb34abb199f9cf23c1cf46
Paul <Paul.etignard@gmail.com>
2022-12-19 19:26:39 +0100

    Ajout de la meteo

--- src/Controller/EtablissementController.php
+++ src/Controller/EtablissementController.php
@@ -4,26 +4,29 @@ namespace App\Controller;
 
 use App\Repository\EtablissementRepository;
 use App\Repository\UserRepository;
-use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
+use GuzzleHttp\Client;
 use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class EtablissementController extends AbstractController
 {
     private EtablissementRepository $etablissementRepository;
     private UserRepository $userRepository;
+    private HttpClientInterface $client;
 
     /**
      * @param EtablissementRepository $etablissementRepository
      */
-    public function __construct(EtablissementRepository $etablissementRepository, UserRepository $userRepository)
+    public function __construct(EtablissementRepository $etablissementRepository, UserRepository $userRepository, HttpClientInterface $client)
     {
         $this->etablissementRepository = $etablissementRepository;
         $this->userRepository = $userRepository;
+        $this->client = $client;
     }
 
 
@@ -42,12 +45,34 @@ class EtablissementController extends AbstractController
     #[Route('/etablissement/{slug}', name: 'app_etablissement_slug')]
     public function EtablissementSlug($slug): Response
     {
-
-
-
         $etablissement = $this->etablissementRepository->findOneBy(["slug"=>$slug]);
+        $nomville = $etablissement->getVille()->getNom();
+
+        $response = $this->client->request(
+            'GET',
+            'http://api.openweathermap.org/geo/1.0/direct?q='.$nomville.'&appid=bc337001363c1789985b1a99d552587d'
+        );
+
+        $response = explode(",",$response->getContent());
+        $reponse1 = explode(":",$response[1]);
+        $lat = $reponse1[1];
+        $reponse2 = explode(":",$response[2]);
+        $long = $reponse2[1];
+
+        $Request2 = $this->client->request(
+            'GET',
+            'https://api.openweathermap.org/data/2.5/weather?lat='.$lat.'&lon='.$long.'&lang=fr&appid=bc337001363c1789985b1a99d552587d'
+        );
+
+        $meteoexpode = explode(",",$Request2->getContent());
+
+        $Temp = explode(":",$meteoexpode[4]);
+        dd($meteoexpode);
+
+
         return $this->render('etablissement/etablissement.html.twig', [
-            "Etablissement" => $etablissement
+            "Etablissement" => $etablissement,
+            "temp"=>$Temp[1]
         ]);
 
 
